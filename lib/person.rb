fdf90596e62842b8b5c5c8c3096a401d10f81672
prmoreira23 <prm.gredes@gmail.com>
2018-05-16 16:21:25 -0400

    Implement all missing methods and pass all tests

--- lib/person.rb
+++ lib/person.rb
@@ -54,4 +54,25 @@ class Person
     "♪ another one bites the dust ♫"
   end
 
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+      when 'politics'
+        self.happiness -= 2
+        person.happiness -= 2
+         "blah blah partisan blah lobbyist"
+      when 'weather'
+        self.happiness += 1
+        person.happiness += 1
+        'blah blah sun blah rain'
+      else
+        'blah blah blah blah blah'
+    end
+  end
+
 end
