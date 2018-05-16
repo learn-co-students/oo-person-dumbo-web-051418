1e1e40135cf286acbd3441402353fd9548e98602
github username <github email address>
2018-05-16 16:07:20 -0400

    halfway there

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,57 @@
-# your code goes here
+class Person
+
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(points)
+  if points < 0
+    @happiness = 0
+  elsif points > 10
+    @happiness = 10
+  else
+    @happiness = points
+    end
+  end
+
+  def hygiene=(index)
+    if index > 10
+      @hygiene = 10
+    elsif index < 0
+      @hygiene = 0
+    else
+      @hygiene = index
+    end
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+end
