db3d867464fa8a33bd5a650dab89b60056ce9aa1
AyyLam <al3101@nyu.edu>
2018-05-16 15:36:11 -0400

    created Person class, initialized, clean?, happy?

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+require 'pry'
+class Person
+
+  attr_reader :name, :hyginene, :happiness
+
+  def initialize(name)
+    @name = name
+    @bank_acc = 25
+    @happiness = 8
+    @hyginene = 8
+  end
+
+  def clean?
+    return self.hyginene > 7
+  end
+
+  def happy?
+    return self.happiness > 7
+  end
+
+
+
+end
