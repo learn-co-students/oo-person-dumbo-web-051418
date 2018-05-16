28d664b6d4f73590d14f33078de07526faf60267
github username <github email address>
2018-05-16 15:53:11 -0400

    add more methods for the person class

--- lib/person.rb
+++ lib/person.rb
@@ -2,36 +2,85 @@ class Person
   attr_reader :name
   attr_accessor :bank_account
 
-  def initialize(name, bank_account=25)
+  def initialize(name, bank_account=25, happiness = 8, hygiene = 8)
     @name = name
     @bank_account = bank_account
-    @happiness_points = 8
-    @hygiene_points = 8
+    @happiness = happiness
+    @hygiene = hygiene
   end
 
-  def happiness_points=(happiness_points)
-    if ![0..10].include?(happiness_points)
-      puts "Your happiness points must be between 0-10."
+  def happiness=(happiness)
+    if happiness > 10
+      @happiness = 10
+    elsif happiness < 0
+      @happiness = 0
     else
-      @happiness_points = happiness_points
+      @happiness = happiness
     end
   end
 
-  def happiness_points
-    @happiness_points
+  def happiness
+    @happiness
   end
 
-  def hygiene_points=(hygiene_points)
-    if ![0..10].include?(hygiene_points)
-      puts "Your hygiene points must be between 0-10."
+  def hygiene=(hygiene)
+    if hygiene > 10
+      @hygiene = 10
+    elsif hygiene < 0
+      @hygiene = 0
     else
-      @hygiene_points = hygiene_points
+      @hygiene = hygiene
     end
   end
 
-  def hygiene_points
-    @hygiene_points
+  def hygiene
+    @hygiene
   end
 
-  
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(starter, topic)
+    case topic
+    when "politics"
+      self.happiness -= 2
+      starter.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness += 1
+      starter.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+
 end
